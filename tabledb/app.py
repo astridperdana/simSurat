89d812d7a527b404c1ef3a2623e0a0186a02d9d8
Octgi Ristya Perdana <astridperdana@gmail.com>
2017-12-14 05:10:05 +0700

    Add files via upload
    init, baru nambah in tentang user

---
+++ tabledb/app.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, request, redirect
+from models import User
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    import sqlite3, os
+    databaseName = os.getcwd() + '/database.db'
+    conn = sqlite3.connect(databaseName)
+    cursor = conn.cursor()
+    container = []
+    for username, password, flag in cursor.execute('SELECT * FROM users'):
+        model = User(username, password, flag)
+        container.append(model)
+    cursor.close()
+    conn.close()
+    return render_template('index.html', container = container)
+
+@app.route('/tambahUser', methods = ['POST', 'GET'])
+def tambah():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        flag = int(request.form['flag'])
+        model = User(username, password, flag)
+        model.tambah()
+        return redirect('http://localhost:5000')
+    else:
+        return render_template('tambah_form.html')
+
+@app.route('/ubahUser/<id>', methods = ['POST', 'GET'])
+def ubah(id):
+    model = User()
+    model.load(id)
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        flag = int(request.form['flag'])
+        model.setUsername(username)
+        model.setPassword(password)
+        model.setFlag(flag)
+        model.ubah()
+        return redirect('http://localhost:5000')
+    else:
+        return render_template('ubah_form.html', model=model)
+
+@app.route('/hapusUser/<id>')
+def hapus(id):
+    model = User()
+    model.load(id)
+    model.hapus()
+    return redirect('http://localhost:5000')
+
+@app.route('/loginUser', methods = ['POST', 'GET'])
+def login():
+    model = User()
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        model.setUsername(username)
+        model.setPassword(password)
+        if model.auth():
+            return render_template('login_sukses.html', username = model.username)
+        else:
+            return render_template('login_gagal.html')
+    else:
+        return render_template('login_form.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ tabledb/models.py
@@ -0,0 +1,63 @@
+import sqlite3, os
+
+DB = os.getcwd() + '/database.db'
+
+class User(object):
+    def __init__(self, username = '', password = '', flag = 0):
+        self.username = username
+        self.password = password
+        self.flag = flag
+
+    def setUsername(self, username):
+        self.username = username
+    
+    def setPassword(self, password):
+        self.password = password
+
+    def setFlag(self, flag):
+        self.flag = flag
+
+    def tambah(self):
+        conn = sqlite3.connect(DB)
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO users VALUES(?,?,?)", (self.username, self.password, self.flag))
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+    def ubah(self):
+        conn = sqlite3.connect(DB)
+        cursor = conn.cursor()
+        cursor.execute('''
+        UPDATE users SET password = ?, flag = ?
+        WHERE username = ?
+        ''', (self.password, self.flag, self.username))
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+    def hapus(self):
+        conn = sqlite3.connect(DB)
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM users WHERE username=?", (self.username))
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+    def load(self, id):
+        conn = sqlite3.connect(DB)
+        cursor = conn.cursor()
+        for username, password, flag in cursor.execute("SELECT * FROM users"):
+            if username == id:
+                self.username = username
+                self.password = password
+                self.flag = flag
+                break
+        cursor.close()
+        conn.close()
+
+    def auth(self):
+        if self.username == 'haha' and self.password == 'haha':
+            return True
+        else:
+            return False
